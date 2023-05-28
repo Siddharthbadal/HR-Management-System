5dad23aed8c5c8f3f05b6c4de5d4721a2fcb8729
siddharthbadal <siddharthabadal.2015@gmail.com>
2023-05-28 12:36:00 +0530

    added new files

--- Candidates/settings.py
+++ Candidates/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m_fgphd3i5eoqu$e98@tz*li*^@80*bv7eit=dj%-s&-cf%cw@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['web-production-e415.up.railway.app', '127.0.0.1']
 
 
 # Application definition
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
